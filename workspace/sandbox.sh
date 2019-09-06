f41f6d2aece4f8fef0de6a702cf4542367968343
Sipley <Breanna.Sipley@gmail.com>
2019-09-06 12:25:03 -0700

    installing dependencies

--- workspace/sandbox.sh
+++ workspace/sandbox.sh
@@ -1,1 +1,5 @@
+# Will call R session information and save as 'sessionInfo.txt'
 Rscript -e 'sessionInfo()' > sessionInfo.txt
+
+# Install dependencies; got mirror error, so added 'repos'
+Rscript -e 'install.packages(c("devtools"), repos = "http://cran.us.r-project.org");if (!all(c("devtools") %in% installed.packages())) { q(status = 1, save = "no")}' > dependencies.txt
